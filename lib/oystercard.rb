4f9665c2f3f30319890119d7cd43c195358ded62
Jacob Cho <jacobcho@live.com>
2021-09-27 15:12:23 +0100

    balance max option added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,23 @@
 class Oystercard
+  MAX_BALANCE = 90
+
   attr_accessor :balance
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise "Maximum balance is #{MAX_BALANCE}" if @balance + amount > 90
     @balance += amount
   end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  # private
+  # def over_max?
+  #   @balance > 90
+  # end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,7 +11,21 @@ describe Oystercard do
       subject.top_up(20) 
       expect(subject.balance).to eq 20
     end
+
+    it "should raise error when balance exceeds limit" do
+      expect {subject.top_up(91)}.to raise_error "Maximum balance is #{Oystercard::MAX_BALANCE}"
+    end
   end
+
+  describe "#deduct" do
+    it "should allow deduction from the balance" do
+      subject.top_up(80)
+      subject.deduct(20) 
+      expect(subject.balance).to eq 60
+    end
+  end
+
+
 
   
 end
