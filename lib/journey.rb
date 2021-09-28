cebd8f9c52af06494f3dea0dafaa818d4c628cbf
Bernard Leanse <bernardleanse96@gmail.com>
2021-09-28 16:51:58 +0100

    started Journey

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,3 +1,10 @@
 class Journey
-  
+  attr_reader :entry_station, :exit_station, :fare
+  DEFAULT_FARE = 1
+  def initialize(entry_station = nil, exit_station = nil)
+    @entry_station = entry_station
+    @exit_station = exit_station
+    @fare = DEFAULT_FARE
+  end
+ 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,4 +1,23 @@
 require 'journey'
 
 describe Journey do
+  let(:station1) { double :station }
+  let(:station2) { double :station }
+  
+  it "should accept entry station" do
+    journey = Journey.new(station1)
+    expect(journey.entry_station).to eq station1
+  end
+
+  it "should accept exit station" do
+    journey = Journey.new(station1, station2)
+    expect(journey.exit_station).to eq station2
+  end
+
+  it "should have default fare" do
+    expect(subject.fare).to eq Journey::DEFAULT_FARE
+  end
+
+  it "should "
 end
+
