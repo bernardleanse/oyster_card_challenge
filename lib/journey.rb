d84f3f9a96ed11cd43d6b5797c51050f88280420
Bernard Leanse <bernardleanse96@gmail.com>
2021-09-29 12:03:52 +0100

    added penalty fare to the thingy

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,10 +1,10 @@
 class Journey
   attr_reader :entry_station, :exit_station, :fare
-  DEFAULT_FARE = 1
+  PENALTY_FARE = 1
   def initialize(entry_station = nil, exit_station = nil)
     @entry_station = entry_station
     @exit_station = exit_station
-    @fare = DEFAULT_FARE
+    @fare = PENALTY_FARE
   end
  
 end
